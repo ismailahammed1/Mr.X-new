24187b42abe626e249d35d045dd3cb3c59036144
Md Ismail Ahammed Roman <ismailahmmedroman@gmail.com>
2023-04-25 04:06:19 +0600

    reademe fil

--- index.js
+++ index.js
@@ -18,6 +18,26 @@ app.get("/mrx-news", (req, res) => {
   res.send(news);
 });
 
+app.get("/categories/:id", (req, res) => {
+  const id = req.params.id;
+  if (id === "08") {
+    res.send(news);
+  } else {
+    const category_news = news.filter((n) => n.category_id === id);
+    res.send(category_news);
+  }
+});
+
+app.get("/news", (req, res) => {
+  res.send(news);
+});
+
+app.get("/news/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedNew = news.find((n) => n._id === id);
+  res.send(selectedNew);
+});
+
 app.listen(port, () => {
   console.log(`example app listening ,`, port);
 });
